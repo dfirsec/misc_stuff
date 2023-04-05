c9fe153de8d2887616f3cf36f48b63e5c49d422b
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-04-05 13:37:17 -0400

    Update cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -1,51 +1,50 @@
 #!/bin/bash
 
-set -euo pipefail
-
 YELLOW="\033[1;33m"
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 ENDCOLOR="\033[0m"
 
+log_file="/var/log/maintenance.log"
+
 step() {
     echo -e "${YELLOW}[+]${ENDCOLOR} ${1}"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [+] ${1}" >>"$log_file"
 }
 
 error() {
     echo -e "${RED}[-]${ENDCOLOR} ${1}"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [-] ${1}" >>"$log_file"
     exit 1
 }
 
 success() {
     echo -e "${GREEN}[+]${ENDCOLOR} ${1}"
-}
-
-check_root() {
-    if [ "$(id -u)" -ne 0 ]; then
-        error "Error: must be root"
-    fi
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [+] ${1}" >>"$log_file"
 }
 
 update_system() {
     step "Updating package lists"
-    apt-get update -q
+    sudo apt-get update -qq || error "Failed to update package lists"
 
     step "Upgrading installed packages"
-    apt-get full-upgrade -y -q
+    sudo apt-get full-upgrade -y -qq || error "Failed to upgrade packages"
 }
 
 clean_apt() {
     step "Cleaning apt cache"
-    apt-get clean -q
-    apt-get autoclean -q
+    sudo apt-get clean -qq || error "Failed to clean apt cache"
+    sudo apt-get autoclean -qq || error "Failed to autoclean apt cache"
 }
 
 remove_old_configs() {
-    local old_configs
-    old_configs=$(dpkg -l | grep "^rc" | awk '{print $2}' || true)
-    if [ -n "$old_configs" ]; then
+    local old_configs=()
+    while read -r config; do
+        old_configs+=("$config")
+    done < <(dpkg -l | grep "^rc" | awk '{print $2}')
+    if [ "${#old_configs[@]}" -gt 0 ]; then
         step "Removing old config files"
-        apt-get purge -y "${old_configs[@]}"
+        sudo apt-get purge -y -qq "${old_configs[@]}" || error "Failed to remove old config files"
     else
         success "No old config files found"
     fi
@@ -53,28 +52,29 @@ remove_old_configs() {
 
 remove_old_kernels() {
     step "Removing old kernels"
-    apt-get --purge autoremove -y -q
+    sudo apt-get --purge autoremove -y -qq || error "Failed to remove old kernels"
 }
 
 remove_old_logs() {
     step "Removing old log files"
-    rm -f /var/log/*gz
+    sudo find /var/log/ -type f -mtime +7 -exec rm -f {} + || error "Failed to remove old log files"
 }
 
 empty_trash() {
     step "Emptying trash"
-    sudo find /home/ -type d ! -perm -g+r,u+r,o+r -prune -o -type d -path '*/.local/share/Trash/*' -exec rm -rf {} +
-    sudo find /root/.local/share/Trash -mindepth 1 -exec rm -rf {} + 2>/dev/null || true
+    sudo find /home/ /root/ -depth -type d \( -path '/home/sift/shared-drives' -o -path '/home/sift/SharedDrives' -o -path '/home/sift/lost+found' \) -prune -o \( -path '/proc' -o -path '/dev' -o -path '/sys' \) -prune -o -type d -name '.*' -prune -o -type d -not -readable -prune -o -type d -path '*/.local/share/Trash/*' -print -delete -print 2>/dev/null
+    sudo find /root/.local/share/Trash -mindepth 1 -delete -print 2>/dev/null
     sudo rm -rf /tmp/*
 }
 
 rotate_journal_logs() {
     step "Rotating journal logs and freeing up space"
-    journalctl --rotate --vacuum-time=1d
+    journalctl --rotate --vacuum-time=1d >/dev/null 2>&1 || error "Failed to rotate journal logs"
 }
 
 main() {
-    check_root
+    echo "" >>"$log_file"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Starting maintenance script" >>"$log_file"
     update_system
     clean_apt
     remove_old_configs
@@ -83,6 +83,13 @@ main() {
     empty_trash
     rotate_journal_logs
     success "Script finished!"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Maintenance script finished" >>"$log_file"
 }
 
-main
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    if [[ $(id -u) -ne 0 ]]; then
+        error "This script must be run with root privileges"
+    fi
+
+    main "$@"
+fi
