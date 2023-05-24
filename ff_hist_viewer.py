026acfa2c88c3a0f6bb49dcb0cfb79ec07237d71
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-05-24 07:11:51 -0400

    Updated

--- ff_hist_viewer.py
+++ ff_hist_viewer.py
@@ -1,12 +1,8 @@
+"""Firefox History & Bookmarks Viewer."""
+
 import argparse
 import sqlite3
 import sys
-
-try:
-    from colorama import Fore, Style, init
-    init()  # Initialize colorama
-except ImportError:
-    sys.exit("\nPlease install colorama: pip install colorama --user\n")
 
 """ Windows Location """
 # C:\Users\<username>\AppData\Roaming\Mozilla\Firefox\Profiles\xxxxxxxx.default\places.sqlite
@@ -19,43 +15,47 @@ except ImportError:
 
 
 class Termcolors:
+    """Terminal Colors."""
+
     # Unicode Symbols and colors
-    CYAN = Fore.CYAN
-    YELLOW = Fore.YELLOW
-    RESET = Fore.RESET
-    ARROW = f"{CYAN}\u279C {RESET}"
-    WARNING = f"{YELLOW}\u03DF {RESET}"
+    cyan = "\033[96m"
+    yellow = "\033[93m"
+    reset = "\033[0m"
+    arrow = f"{cyan}\u279C {reset}"
+    warning = f"{yellow}\u03DF {reset}"
 
 
-def history(db):
+def history(db: str) -> None:
+    """Print Firefox history and bookmarks."""
     tc = Termcolors()
     try:
         conn = sqlite3.connect(db)
         cursor = conn.cursor()
         bookmarks = "SELECT url, moz_places.title, datetime(last_visit_date/1000000, \"unixepoch\") FROM moz_places JOIN moz_bookmarks ON moz_bookmarks.fk=moz_places.id WHERE visit_count >= 0 AND moz_places.url LIKE 'http%' order by dateAdded desc;"
-        history = "SELECT url, datetime(visit_date/1000000, \"unixepoch\") FROM moz_places, moz_historyvisits WHERE visit_count >= 0 AND moz_places.id==moz_historyvisits.place_id;"
+        history = 'SELECT url, datetime(visit_date/1000000, "unixepoch") FROM moz_places, moz_historyvisits WHERE visit_count >= 0 AND moz_places.id==moz_historyvisits.place_id;'
 
-        print(f"{tc.YELLOW}\n --[ Bookmarks ]--{tc.RESET}")
+        print(f"{tc.yellow}\n[ Bookmarks ]{tc.reset}")
         for row in cursor.execute(bookmarks):
             url = str(row[0])
-            bookmark = str(row[1])
-            last_visited = str(row[2])
-            if row[0] and row[2] != None:
-                print(f"{tc.ARROW} {last_visited}: {bookmark}, {url}")
+            if row[0] and row[2] is not None:
+                bookmark = str(row[1])
+                last_visited = str(row[2])
+                print(f"{tc.arrow} {last_visited}: {bookmark}, {url}")
             else:
-                print(f"{tc.ARROW} {url}")
+                print(f"{tc.arrow} {url}")
 
-        print(f"{tc.YELLOW}\n --[ History ]--{tc.RESET}")
+        print(f"{tc.yellow}\n[ History ]{tc.reset}")
         for row in cursor.execute(history):
             url = str(row[0])
             date = str(row[1])
-            print(f"{tc.ARROW} {date}: {url}")
+            print(f"{tc.arrow} {date}: {url}")
 
     except Exception as err:
-        sys.exit(f"{tc.WARNING} Error reading database. {err}")
+        sys.exit(f"{tc.warning} Error reading database. {err}")
 
 
-def main():
+def main() -> None:
+    """Main function."""
     parser = argparse.ArgumentParser("Firefox History & Bookmarks Viewer")
     parser.add_argument("file", help="Firefox sqlite file path")
     args = parser.parse_args()
