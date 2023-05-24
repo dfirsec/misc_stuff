2d516f6f9b0a10fb5373388859e50ada327d4b57
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-05-24 07:03:00 -0400

    Updated

--- url_expander.py
+++ url_expander.py
@@ -1,21 +1,24 @@
+"""Expand short URLs and check reputation of source domain."""
+
 import sys
 from urllib.parse import urlparse
 
 import requests
 from dns import resolver, flags
 
-CYN = "\033[36m"
-RED = "\033[31m"
-YEL = "\033[33m"
-RST = "\033[0m"
+cyan = "\033[36m"
+red = "\033[31m"
+yellow = "\033[33m"
+reset = "\033[0m"
 
 if len(sys.argv) < 2:
-    sys.exit(f"\n{CYN}Usage:{RST} python url_expander.py <URL>")
+    sys.exit(f"\n{cyan}Usage:{reset} python url_expander.py <URL>")
 else:
     url = sys.argv[1]
 
 
-def quad9(domain):
+def quad9(domain: str) -> str | None:
+    """Return reputation of domain from Quad9 DNS server."""
     # ref: https://www.quad9.net/support/faq/#testing
     dns_resp = None
     resolve = resolver.Resolver()
@@ -23,27 +26,28 @@ def quad9(domain):
     try:
         dns_resp = "non-malicious"
     except resolver.NXDOMAIN as error:
-        for (_, resp) in error.responses().items():  # type: ignore
+        for _, resp in error.responses().items():  # type: ignore
             recursion = resp.flags & flags.RA  # type: ignore
             dns_resp = "malicious" if recursion == 0 else "NXDOMAIN"
     except (resolver.NoAnswer, resolver.Timeout):
         pass
     except resolver.NoNameservers:
-        dns_resp = f"{RED}[x]{RST} Failed to resolve domain {domain}"
+        dns_resp = f"{red}[x]{reset} Failed to resolve domain {domain}"
 
     return dns_resp
 
 
-def expand_url(short_url):
+def expand_url(short_url: str) -> requests.Response:
+    """Return expanded URL from short URL."""
     session = requests.Session()
     session.max_redirects = 3
     timeout_value = 2.5
     try:
         response = session.head(short_url.strip(), timeout=timeout_value, allow_redirects=True)
     except requests.exceptions.ConnectionError:
-        sys.exit(f"{RED}[x]{RST}{YEL} Connection Error:{RST} {short_url}")
+        sys.exit(f"{red}[x]{reset}{yellow} Connection Error:{reset} {short_url}")
     except requests.exceptions.MissingSchema as error:
-        sys.exit(f"{RED}[x]{RST} {error}")
+        sys.exit(f"{red}[x]{reset} {error}")
     else:
         return response
 
@@ -51,9 +55,11 @@ def expand_url(short_url):
 results = expand_url(url)
 expanded = results.url.strip()
 src_domain = urlparse(results.url.strip()).netloc
+quad9_result = quad9(src_domain)
 
 if results.history:
-    print(f"{CYN}[+] {'History:':12}{RST}{' | '.join([results.url for results in results.history])}")
+    print(f"{cyan}[+] {'History:':12}{reset}{' | '.join([results.url for results in results.history])}")
 
-print(f"{CYN}[+] {'Expanded:':12}{RST}{expanded}")
-print(f"{CYN}[+] {'Reputation:':12}{RST}{quad9(src_domain).title()}")
+print(f"{cyan}[+] {'Expanded:':12}{reset}{expanded}")
+if quad9_result is not None:
+    print(f"{cyan}[+] {'Reputation:':12}{reset}{quad9_result.title()}")
