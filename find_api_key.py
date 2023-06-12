f3ccf64f79b39ae544b50eedb377d0b5cbb430da
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-06-12 09:14:26 -0400

    Update find_api_key.py

--- find_api_key.py
+++ find_api_key.py
@@ -1,5 +1,4 @@
-"""Find api_key references in files."""
-
+import contextlib
 import multiprocessing
 import re
 import sys
@@ -7,49 +6,70 @@ from functools import partial
 from pathlib import Path
 
 
-def check_file(file_path: Path, api_key_regexes: dict) -> Path | None:
+def check_file(file_path: Path, api_key_regexes: dict) -> tuple[Path, list[int], list[str]] | None:
     """Check if a file contains 'api_key'."""
     file_ext = file_path.suffix
-    if file_ext in api_key_regexes and api_key_regexes[file_ext].search(file_path.read_text(encoding="utf-8")):
-        return file_path
+    if file_ext in api_key_regexes:
+        line_numbers = []
+        api_keys = []
+        excluded_keys = [
+            "yourshodanapikey",
+            "yourshodankey",
+            "yourapikey",
+            "api_key",
+            "apikey",
+            "api-key",
+        ]
+        with contextlib.suppress(OSError):
+            with file_path.open(encoding="utf-8", errors="ignore") as file:
+                for line_no, line in enumerate(file, start=1):
+                    match = api_key_regexes[file_ext].search(line)
+                    if match:
+                        matched_key = match.group(1).lower()
+                        if matched_key not in excluded_keys:
+                            line_numbers.append(line_no)
+                            api_keys.append(matched_key)
+            if line_numbers and api_keys:
+                return file_path, line_numbers, api_keys
     return None
 
 
-def find_api_key_references(root_dir: str) -> list:
+def find_api_key_references(root_dir: str) -> list[tuple[Path, list[int], list[str]]]:
     """Find api_key references in files."""
     patterns = {
-        ".bashrc": re.compile(r"export\s+API_KEY\s*=\s*.+", re.IGNORECASE),
-        ".cfg": re.compile(r"api_key\s*=\s*.+", re.IGNORECASE),
-        ".conf": re.compile(r"api_key\s*=\s*.+", re.IGNORECASE),
-        ".config": re.compile(r"\bapi_key\s*=\s*\w+", re.IGNORECASE),
-        ".dockerfile": re.compile(r"ENV\s+API_KEY\s*=\s*.+", re.IGNORECASE),
-        ".ini": re.compile(r"api_key\s*=\s*.+", re.IGNORECASE),
-        ".json": re.compile(r"\"api_key\"\s*:\s*\".+\"", re.IGNORECASE),
-        ".php": re.compile(r"\$api_key\s*=\s*\'\w+\'", re.IGNORECASE),
-        ".properties": re.compile(r"api_key\s*=\s*.+", re.IGNORECASE),
-        ".py": re.compile(r'\bapi_key\s*=\s*["\']\w+["\']', re.IGNORECASE),
-        ".toml": re.compile(r"api_key\s*=\s*.+", re.IGNORECASE),
-        ".xml": re.compile(r"<api_key>\s*.*\s*</api_key>", re.IGNORECASE),
-        ".yaml": re.compile(r"\bapi_key\s*:\s*\w+", re.IGNORECASE),
-        ".yml": re.compile(r"\bapi_key\s*:\s*\w+", re.IGNORECASE),
+        ".bashrc": re.compile(r"export\s+API_KEY\s*=\s*(.+)", re.IGNORECASE),
+        ".cfg": re.compile(r"api_key\s*=\s*(.+)", re.IGNORECASE),
+        ".conf": re.compile(r"api_key\s*=\s*(.+)", re.IGNORECASE),
+        ".config": re.compile(r"\bapi_key\s*=\s*(\w+)", re.IGNORECASE),
+        ".dockerfile": re.compile(r"ENV\s+API_KEY\s*=\s*(.+)", re.IGNORECASE),
+        ".ini": re.compile(r"api_key\s*=\s*(.+)", re.IGNORECASE),
+        ".json": re.compile(r"\"api_key\"\s*:\s*\"(.+)\"", re.IGNORECASE),
+        ".php": re.compile(r"\$api_key\s*=\s*\'(\w+)\'", re.IGNORECASE),
+        ".properties": re.compile(r"api_key\s*=\s*(.+)", re.IGNORECASE),
+        ".py": re.compile(r'\bapi_key\s*=\s*["\'](\w+)["\']', re.IGNORECASE),
+        ".toml": re.compile(r"api_key\s*=\s*(.+)", re.IGNORECASE),
+        ".xml": re.compile(r"<api_key>\s*(.*)\s*</api_key>", re.IGNORECASE),
+        ".yaml": re.compile(r"\bapi_key\s*:\s*(\w+)", re.IGNORECASE),
+        ".yml": re.compile(r"\bapi_key\s*:\s*(\w+)", re.IGNORECASE),
     }
-    files_with_api_key = []
 
     with multiprocessing.Pool() as pool:
         files = list(Path(root_dir).rglob("*"))
-        files_with_api_key = pool.map(partial(check_file, api_key_regexes=patterns), files)
+        all_results = pool.map(partial(check_file, api_key_regexes=patterns), files)
 
-    return [file for file in files_with_api_key if file]
+    return [result for result in all_results if result]
 
 
 def main() -> None:
     """Main function."""
     root_dir = input("Enter the root directory to search for 'api_key': ")
     try:
+        print("Searching for 'api_key' in files...")
         files_with_api_key = find_api_key_references(root_dir)
         if files_with_api_key:
-            print("\nFiles containing 'api_key':")
-            print("\n".join(map(str, files_with_api_key)))
+            print("\nFiles containing 'api_key':\n-------------------------------")
+            for file_path, line_numbers, api_keys in files_with_api_key:
+                print(f"{file_path} (lines: {', '.join(map(str, line_numbers))}), API Keys: {', '.join(api_keys)}")
         else:
             print("No files found containing 'api_key'.")
     except KeyboardInterrupt:
