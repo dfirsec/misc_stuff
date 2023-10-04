eef22bb81b971a1dff27b039c15e4dbb81da7c78
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-10-04 06:54:27 -0400

    Update maintenance_script.sh

--- maintenance_script.sh
+++ maintenance_script.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 CYAN="\033[0;36m"
+YELLOW="\033[1;33m"
 RED="\033[0;31m"
 GREEN="\033[0;32m"
-YELLOW="\033[1;33m"
-MAGENTA="\033[0;35m"
 ENDCOLOR="\033[0m"
+
+if [[ $(id -u) -ne 0 ]]; then
+    error "This script must be run with root privileges"
+fi
+
+reboot_required=0
 
 log_file="/var/log/maintenance.log"
 
 step() {
-    echo -e "${CYAN}[*]${ENDCOLOR} ${1}"
+    echo -e "${YELLOW}[+]${ENDCOLOR} ${1}"
     echo "$(date +'%Y-%m-%d %H:%M:%S') [+] ${1}" >>"$log_file"
 }
 
 error() {
     echo -e "${RED}[-]${ENDCOLOR} ${1}"
-    echo "$(date +'%Y-%m-%d %H:%M:%S') [-] ${2:-${1}}" >>"$log_file"
-    return 1
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [-] ${1}" >>"$log_file"
+    exit 1
 }
 
 success() {
@@ -25,34 +30,72 @@ success() {
     echo "$(date +'%Y-%m-%d %H:%M:%S') [+] ${1}" >>"$log_file"
 }
 
-warning() {
-    echo -e "${YELLOW}[!]${ENDCOLOR} ${1}"
-    echo "$(date +'%Y-%m-%d %H:%M:%S') [!] ${1}" >>"$log_file"
+check_filesystem() {
+    step "Forcing fsck on the next boot to check filesystem for errors"
+    sudo touch /forcefsck || error "Failed to create /forcefsck"
+    reboot_required=1
 }
 
-info() {
-    echo -e "${MAGENTA}[i]${ENDCOLOR} ${1}"
-    echo "$(date +'%Y-%m-%d %H:%M:%S') [i] ${1}" >>"$log_file"
+# Check Disk Space
+check_disk_space() {
+    step "Checking disk space"
+    local limit=90 # set the limit as per your need
+    local usage
+    usage=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
+    if [ "$usage" -gt "$limit" ]; then
+        error "Disk space usage is above $limit%"
+    fi
 }
 
+# Check for Failed Systemd Services
+check_failed_services() {
+    step "Checking for failed systemd services"
+    if systemctl --failed | grep 'loaded units listed'; then
+        success "No failed systemd services found"
+    else
+        error "Some systemd services have failed"
+    fi
+}
+
+# Reboot Requirement Check
+check_reboot_required() {
+    step "Checking if a reboot is required"
+    if [ -f /var/run/reboot-required ]; then
+        error "A reboot is required. Please reboot the system at your earliest convenience."
+    else
+        success "No reboot required"
+    fi
+}
+
+# Review Authentication Logs
+review_auth_logs() {
+    local failed_attempts
+    failed_attempts=$(grep -c "Failed password" /var/log/auth.log)
+
+    if [ "$failed_attempts" -gt 20 ]; then
+        error "$failed_attempts failed login attempts found in auth.log"
+    else
+        success "No unusual activity found in auth.log"
+    fi
+}
+
+# Update System
 update_system() {
     step "Updating package lists"
-    if ! output=$(sudo apt-get update -qq 2>&1); then
-        error "Failed to update package lists" "Failed to update package lists: $output"
-    fi
+    sudo apt-get update -qq || error "Failed to update package lists"
 
     step "Upgrading installed packages"
-    if ! output=$(sudo apt-get full-upgrade -y -qq 2>&1); then
-        error "Failed to upgrade packages" "Failed to upgrade packages: $output"
-    fi
+    sudo apt-get full-upgrade -y -qq || error "Failed to upgrade packages"
 }
 
+# Clean
 clean_apt() {
     step "Cleaning apt cache"
-    output=$(sudo apt-get clean -qq 2>&1) || error "Failed to clean apt cache: $output"
-    output=$(sudo apt-get autoclean -qq 2>&1) || error "Failed to autoclean apt cache: $output"
+    sudo apt-get clean -qq || error "Failed to clean apt cache"
+    sudo apt-get autoclean -qq || error "Failed to autoclean apt cache"
 }
 
+# Remove Old Config Files
 remove_old_configs() {
     local old_configs=()
     while read -r config; do
@@ -60,137 +103,134 @@ remove_old_configs() {
     done < <(dpkg -l | grep "^rc" | awk '{print $2}')
     if [ "${#old_configs[@]}" -gt 0 ]; then
         step "Removing old config files"
-        output=$(sudo apt-get purge -y -qq "${old_configs[@]}" 2>&1) || error "Failed to remove old config files: $output"
+        sudo apt-get purge -y -qq "${old_configs[@]}" || error "Failed to remove old config files"
     else
-        info "No old config files found"
+        success "No old config files found"
     fi
 }
 
 remove_old_kernels() {
     step "Removing old kernels"
-    output=$(sudo apt-get --purge autoremove -y -qq 2>&1) || error "Failed to remove old kernels: $output"
+    sudo apt-get --purge autoremove -y -qq || error "Failed to remove old kernels"
 }
 
 remove_old_logs() {
     step "Removing old log files"
-    output=$(sudo find /var/log/ -type f -mtime +7 -exec rm -f {} + 2>&1) || error "Failed to remove old log files: $output"
-}
-
-check_protected() {
-    local file=$1
-    local owner
-    local permissions
-
-    owner=$(stat -c '%U' "$file")
-    permissions=$(stat -c '%A' "$file")
-
-    # Check if the file is owned by root or a system user
-    if [[ "$owner" == "root" || "$owner" == "system" ]]; then
-        return 0
-    fi
-
-    # Check if the file has restrictive permissions
-    if [[ "$permissions" =~ ^[drwx-]{3}[-]{3} ]]; then
-        return 0
-    fi
-
-    return 1
+    sudo find /var/log/ -type f -mtime +7 -exec rm -f {} + || error "Failed to remove old log files"
 }
 
 empty_trash() {
     step "Emptying trash"
-    if ! output=$(find /home/ /root/ -depth -type d \( -path '*/shared-drives' -o -path '*/SharedDrives' -o -path '*/lost+found' -o -path '/mnt' -o -path '/media' -o -name '.*' \) -prune -o -type d -path '*/.local/share/Trash/*' -delete -print 2>&1); then
-        error "Failed to empty trash: $output"
-    fi
 
-    if [ -d /root/.local/share/Trash ]; then
-        if [ -z "$(ls -A /root/.local/share/Trash)" ]; then
-            success "Successfully emptied root trash"
-        else
-            error "root trash was not emptied"
-            echo "$(date +'%Y-%m-%d %H:%M:%S') [i] Contents of trash at /root/.local/share/Trash after emptying: $(ls /root/.local/share/Trash)" >>"$log_file"
-        fi
-    else
-        info "Trash at /root/.local/share/Trash does not exist"
-    fi
-
-    step "Checking file/directory protections"
-
-    shopt -s nullglob dotglob # Enable nullglob and dotglob to include hidden files and directories
-    for file in /tmp/*; do
-        if [[ -f "$file" || -d "$file" ]]; then
-            if check_protected "$file"; then
-                info "Protected: $file (skipping removal)"
-                continue
-            fi
-
-            echo "Removing $file..."
-            output=$(rm -rf "$file") || error "Failed to empty /tmp: $output"
+    # Empty trash directories for all users in /home/
+    for user_dir in /home/*; do
+        if [ -d "$user_dir/.local/share/Trash" ]; then
+            rm -rf "$user_dir/.local/share/Trash"/*
         fi
     done
-    shopt -u nullglob dotglob # Disable nullglob and dotglob after loop completion
+
+    # Empty trash for root user
+    rm -rf /root/.local/share/Trash/*
+
+    # Clear the /tmp/ directory
+    rm -rf /tmp/*
+}
+
+# NTP Sync
+ntp_sync() {
+    step "Synchronizing system clock with NTP servers"
+    sudo ntpdate pool.ntp.org || error "Failed to sync with NTP"
 }
 
 rotate_journal_logs() {
-    if [ -d /run/systemd/journal/ ]; then
-        step "Rotating journal logs and freeing up space"
-        output=$(journalctl --rotate --vacuum-time=1d 2>&1) || error "Failed to rotate journal logs: $output"
-    else
-        error "Failed to rotate journal logs: Journal service may not be running"
-    fi
+    step "Rotating journal logs and freeing up space"
+    journalctl --rotate --vacuum-time=1d >/dev/null 2>&1 || error "Failed to rotate journal logs"
 }
 
-run_all() {
-    echo -e "\nRunning all maintenance tasks...\n"
+adjust_clock() {
+    step "Adjusting hardware clock"
+    echo -e "${CYAN}[i]${ENDCOLOR} Current clock: $(date)"
+    sudo hwclock --hctosys
+    echo -e "${CYAN}[i]${ENDCOLOR} Updated clock: $(date)"
+}
+
+flush_dns() {
+    step "Flushing DNS cache"
+    sudo systemd-resolve --flush-caches
+}
+
+complete_maintenance() {
+    echo "" >>"$log_file"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Starting maintenance script" >>"$log_file"
+    echo "Running all maintenance tasks..."
+    ntp_sync
     update_system
+    rotate_journal_logs
+    check_failed_services
+    check_reboot_required
+    review_auth_logs
     clean_apt
     remove_old_configs
     remove_old_kernels
-    remove_old_logs
     empty_trash
-    rotate_journal_logs
-
+    remove_old_logs
+    check_filesystem
+    adjust_clock
+    flush_dns
     success "All maintenance tasks completed!"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Maintenance script finished" >>"$log_file"
 }
 
 main_menu() {
-    echo ""
-    echo "Maintenance Script"
-    echo "==================="
-    echo "1. Update system"
-    echo "2. Clean apt"
-    echo "3. Remove old config files"
-    echo "4. Remove old kernels"
-    echo "5. Remove old log files"
-    echo "6. Empty trash"
-    echo "7. Rotate journal logs"
-    echo "8. All"
-    echo "0. Exit"
+    while true; do
+        echo ""
+        echo "Maintenance Script"
+        echo "==================="
+        echo "1. Update system"
+        echo "2. Clean apt"
+        echo "3. Remove old config files"
+        echo "4. Remove old kernels"
+        echo "5. Remove old log files"
+        echo "6. Empty trash"
+        echo "7. Rotate journal logs"
+        echo "8. Adjust hardware clock"
+        echo "9. Check Filesystem Errors"
+        echo "10. Check Failed Systemd Services"
+        echo "11. Check Reboot Requirement"
+        echo "12. Review Authentication Logs"
+        echo "13. NTP Sync"
+        echo "14. All - Complete Maintenance"
+        echo "0. Exit"
 
-    read -rp "Enter your choice: " choice
+        read -rp "Enter your choice: " choice
 
-    case $choice in
-    1) update_system ;;
-    2) clean_apt ;;
-    3) remove_old_configs ;;
-    4) remove_old_kernels ;;
-    5) remove_old_logs ;;
-    6) empty_trash ;;
-    7) rotate_journal_logs ;;
-    8) run_all ;;
-    0)
-        echo "Exiting..."
-        exit
-        ;;
-    *) echo "Invalid choice" ;;
-    esac
-
-    main_menu
+        case $choice in
+        1) update_system ;;
+        2) clean_apt ;;
+        3) remove_old_configs ;;
+        4) remove_old_kernels ;;
+        5) remove_old_logs ;;
+        6) empty_trash ;;
+        7) rotate_journal_logs ;;
+        8) adjust_clock ;;
+        9) check_filesystem ;;
+        10) check_failed_services ;;
+        11) check_reboot_required ;;
+        12) review_auth_logs ;;
+        13) ntp_sync ;;
+        14) complete_maintenance ;;
+        0)
+            echo "Exiting..."
+            break
+            ;;
+        *) echo "Invalid choice" ;;
+        esac
+    done
 }
 
-if [[ $(id -u) -ne 0 ]]; then
-    echo -e "${RED}[-] This script must be run with root privileges${ENDCOLOR}"
-    exit 1
-else
-    main_menu
+# Call the main_menu function
+main_menu
+
+if [ $reboot_required -eq 1 ]; then
+    echo -e "${RED}[!]${ENDCOLOR} It's recommended to reboot the system for the changes to take effect."
 fi
